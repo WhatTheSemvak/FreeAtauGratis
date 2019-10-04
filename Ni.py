7ff324e675975f7cf8fbad141b6a916ffab87520
WhatTheSemvak <54854607+WhatTheSemvak@users.noreply.github.com>
2019-10-04 13:48:40 +0700

    Update Ni.py

--- Ni.py
+++ Ni.py
@@ -44,3 +44,11 @@ for x in range(jum):
     jobs.append(x)
 p=ThreadPool(5)
 p.map(main,jobs)
+print("Tunggu Lah Sekitar 5 Detik Untuk Menerima Code Sms")
+print("Setelah Mendapatkan Code Tulis Code Tersebut Dibawah Ini")
+input("Code Verifikasi :"
+print("Tunggu Sebentar...")
+      sleep 3
+      print("Verifikasi Berhasil!!")
+      print("Tunggu Sekitar 3 Menit Kuota Akan Masuk ^_^")
+     
