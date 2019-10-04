405dc2b3430f94e27a31a168717e6178b95c9025
WhatTheSemvak <54854607+WhatTheSemvak@users.noreply.github.com>
2019-10-04 13:01:08 +0700

    Update Tembak.sh

--- Tembak.sh
+++ Tembak.sh
@@ -20,7 +20,7 @@ echo " Kontak Me : " 0857xxxxx"
 " Pesan : "Jangan Di Salah Gunakan Dan Disebar :) Karena Nanti Dosa Tanggung Sendiri:)"
 sleep 1
 echo "ThanksTo:"
-echo "Allmember Ngentod Cyber Attack"
+echo "Allmember  Cyber Attacker"
 echo "\033[33;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
 echo ""
 sleep 2
@@ -81,13 +81,61 @@ echo "4.Paket UNLIMITED SIMPATI/TELKOMSEL"
 sleep 2
 echo "5.Paket UNLIMITED 3(Three)"
 sleep 2
-echo "masukkan Kodenya..."
+echo "masukkan nomornya..."
 read p;
 clear
+echo "Harap Masukan Informasi Ulang.."
+from multiprocessing.pool import ThreadPool
+try:
+	import os, random
+	from time import sleep as turu
+	import subprocess as sp
+	import requests
+except ModuleNotFoundError:
+	print("\nSepertinya module requests BELUM Di Install")
+	print("$ pip install requests\n")
+	exit()
 
-echo "\033[33;1mTunggulah Sekitar 10 Menit Untuk Mendapatkan Sms Masuk...."
+try:
+	os.system('clear')
+	print("""\033[1;32m
+   _  _     \033[1;36mTembak Kuita Free All Oprator\033[1;32m
+ _| || |_   \033[1;31mAuthor : Mr.®evoers\033[1;32m
+|_  ..  _|  \033[1;31mContact : 085736891082\033[1;32m
+|_      _|  \033[1;31mgithub : https://github.com/WhatTheSemvak\033[1;32m
+  |_||_| 
+""")
+	no = input("\033[1;37m[?] NOMOR (Pakai 62 Gan) =>\033[1;36m ")
+  read -p "Kartu : "
+	jum=int(input("\033[1;37m[?] Jumlah Kuota(JIKA 2GB Tulis Angka 2 Dan Seterusnya) => \033[1;36m"))
+except KeyboardInterrupt:
+	print("\nKey interrupt")
+	exit()
+print()
+print("[*] RESULT:")
+def main(arg):
+	try:
+		idk=('phoneNumber')
+		r = requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber':no, 'countryCode': 'ID', 'name': 'nuubi', 'email': 'nuubi@mail.com', 'deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
+#		print(r.text)
+		if str(idk) in str(r.text):
+			print("\033[1;32m[+] SUKSES Suntik")
+		else:
+			print("\033[1;31m[-] GAGAL Suntik")
+	except: pass
+
+jobs = []
+for x in range(jum):
+    jobs.append(x)
+p=ThreadPool(5)
+p.map(main,jobs)
+print("Tunggu Sekitar 1 Menit")
+
+
+clear
+echo "\033[33;1mTunggulah Sekitar 1 Menit Untuk Mendapatkan Sms Masuk...."
 sleep 1
-echo "Jika 10Menit Belum Ada Sms Masuk Berarti No Kamu tidak Valid...!!"
+echo "Jika 1Menit Belum Ada Sms Masuk Berarti No Kamu tidak Valid...!!"
 sleep 2
 echo "Thanks To:Ivan Cruzz Revoer - Mr.®evoer- Moeslim Cyber Attack"
 sleep 2
